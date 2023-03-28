7fd33682ac4a7001c7ee56d0b9b12b9cfeac5327
Tuna <df95111111@gmail.com>
2023-03-28 20:34:27 +0800

    feat: 加入基礎的admin index頁面

---
+++ resources/views/layouts/adminlte.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>@yield('title')</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    {{-- <link rel="stylesheet" href="{{ asset('app.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/adminlte.css') }}">
+    @yield('css')
+</head>
+
+<body class="hold-transition @yield('body-class')">
+    <div class="wrapper">
+        @yield('content')
+    </div>
+    {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+    <script src="{{ asset('js/adminlte.js') }}"></script>
+    @yield('js')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,9 +20,7 @@ Route::get('/', function () {
     return redirect()->route('dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [AdminController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
